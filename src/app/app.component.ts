88b5cc0fa9b122d2360917298764c2773cf73183
djangojazz <brettmorin2@gmail.com>
2018-08-09 09:16:04 -0700

    Update changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'AngularResume Update';
+  title = 'Brett Morin - Skills';
+
+  public navChanged(nav: string) {
+    this.title = `Brett Morin - ${nav}`;
+  }
 }
