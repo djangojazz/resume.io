d1237d66425e6b40f426041f6fc5576396edaf60
djangojazz <brettmorin2@gmail.com>
2018-08-09 16:37:57 -0700

    Updated some styling to look better

--- src/app/page-not-found.component.ts
+++ src/app/page-not-found.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
 @Component({ 
-    template: `<h2 class="warn">Page not found</h2>`
+    template: `<h2 class="warning">Page not found</h2>`
 })
 export class PageNotFoundComponent { }
